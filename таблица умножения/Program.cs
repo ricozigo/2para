89a8bc97334b522d07139464429a78e039108721
иртвошыалфывмпайфмйуампйукаимпйепаийепаит2кйцеи <Pisa@com>
2023-09-07 11:44:05 +0700

    Добавьте файлы проекта.

---
+++ таблица умножения/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число для генерации таблицы умножения: ");
+        for (int number = 1; number <= 10; number++)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                int result = number * i;
+                Console.WriteLine($"{number} x {i} = {result}");
+            }
+        }
+
+        Console.WriteLine();
+
+
+
+        Console.ReadLine(); // Чтобы консольное окно не закрывалось сразу после вывода таблицы
+    }
+}
+
